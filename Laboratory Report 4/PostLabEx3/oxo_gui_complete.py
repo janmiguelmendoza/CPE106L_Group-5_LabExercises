9b34d1e9f8a81d0efd7981da00c2cc4daba8a663
vfri1 <vmrchua@mymail.mapua.edu.ph>
2025-02-05 00:29:38 +0800

    Update oxo_gui_complete.py

--- Laboratory Report 4/PostLabEx3/oxo_gui_complete.py
+++ Laboratory Report 4/PostLabEx3/oxo_gui_complete.py
@@ -1,1 +1,37 @@
+import unittest
+import oxy_logic
+import oxy_data
 
+class TestOxyGame(unittest.TestCase):
+    
+    def test_new_game(self):
+        game = oxy_logic.newGame()
+        self.assertEqual(len(game), 9)
+    
+    def test_save_and_restore_game(self):
+        game = ["X", "O", "X", "O", "X", "O", "X", "O", " "]
+        oxy_data.saveGame(game)
+        restored_game = oxy_data.restoreGame()
+        self.assertEqual(game, restored_game)
+
+    def test_generate_move(self):
+        game = ["X", "O", " ", " ", "X", " ", "O", " ", "X"]
+        move = oxy_logic._generateMove(game)
+        self.assertIn(move, [i for i, v in enumerate(game) if v == " "])
+
+    def test_winning_move(self):
+        game = ["X", "X", "X", "O", "O", " ", " ", " ", " "]
+        self.assertTrue(oxy_logic._isWinningMove(game))
+
+    def test_user_move(self):
+        game = oxy_logic.newGame()
+        oxy_logic.userMove(game, 0)    
+        self.assertEqual(game[0], "X")  
+    
+    def test_computer_move(self):
+        game = ["X", "O", "X", "O", "X", "O", "X", "O", " "]
+        result = oxy_logic.computerMove(game)
+        self.assertEqual(result, "O")  
+
+if __name__ == "__main__":
+    unittest.main()
